28ae6c684be2bab87e85b2325ce73c0bc2de7fbf
Gabriel Torres <GabrielT1522@gmail.com>
2022-09-16 10:50:37 -0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+Name: Gabriel Alexander Torres
+Class: CSCE 3301
+Title: Towers of Hanoi
+
+This program uses an applied implementation of the Linked List based Stack.
+
+After creating a linked list based stack, the program prompts the user to participate
+in a game of Towers of Hanoi. The game has the following restrictions:
+
+    1. Only one disk can be moved at a time.
+    2. Each move consists of taking the upper disk from one of the stacks and placing it on
+       top of another stack. In other words, a disk can only be moved if it is the uppermost
+       disk on a stack.
+    3. No larger disk may be placed on top of a smaller disk.
---
+++ main.cpp
@@ -0,0 +1,97 @@
+// CSCE 3301 - Algorithms and Data Structures
+// main.cpp
+// TowersOfHanoi
+//
+// Created by Gabriel Torres on 9/12/22.
+
+#include <iostream>
+using namespace std;
+
+// Create Node Link
+class Node{
+public:
+    int element;
+    Node* next;
+};
+
+// Create Linked List based Stack
+class LinkedListStack{
+public:
+    LinkedListStack(){
+        head = new Node;
+        tail = new Node;
+        head->next = tail;
+        count = 0;
+    }
+    void push(int element){
+        Node* newNode = new Node();
+        newNode->element = element;
+        newNode->next = head;
+        head = newNode;
+        count++;
+    }
+    int pop(){
+        if (empty()){
+            cout << "Can not pop. Stack is empty." << endl;
+            exit(0);
+        }
+        int nodeElement = head->element;
+        Node* tempNode = head;
+        head = head->next;
+
+        delete(tempNode);
+        count--;
+        return nodeElement;
+    }
+
+    int top(){
+        return head->element;
+    }
+    bool empty(){
+        if (count==0)
+            return true;
+        else
+            return false;
+    }
+    int size(){
+        return count;
+    }
+    void display(){
+        if (empty()){
+            cout << "Can not display. Stack is empty." << endl;
+            return;
+        }
+
+        Node *tempNode = head;
+        while (tempNode != tail){
+            cout << tempNode->element << " >< ";
+            tempNode = tempNode->next;
+        }
+        cout << endl;
+    }
+
+private:
+    Node* head;
+    Node* tail;
+    int count;
+};
+
+// Main Program - Towers of Hanoi
+int main() {
+    LinkedListStack stack;
+
+    stack.push(5);
+    stack.push(9);
+    stack.push(7);
+    stack.push(9);
+    stack.push(10);
+    cout << "Current stack size: " << stack.size() << endl;
+    stack.display();
+    stack.pop();
+    stack.pop();
+    cout << "Current stack size: " << stack.size() << endl;
+    cout << "Current top: " << stack.top() << endl;
+    stack.display();
+
+    return 0;
+}
